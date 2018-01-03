c612c30b99d473f051682e9e5eb18bdb18e7957f
Rune Piper <kontakt@runepiper.de>
2018-01-03 20:45:27 +0100

    [CLEAMUP] Follow PSR-2 guidelines

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,6 @@
 defined('TYPO3_MODE') or die();
 
 $boot = function () {
-
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Backend\Form\Container\PaletteAndSingleContainer::class] = [
         'className' => \GeorgRinger\BackendDebug\Xclass\PaletteAndSingleContainer::class,
     ];
